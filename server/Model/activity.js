94c968d475ac4c81fbcac439d7906a2e593751a8
Aisha Gul <aishagulahmed@gmail.com>
2023-01-15 13:42:00 +0500

    model for activity tracker

---
+++ server/Model/activity.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var activitySchema = new Schema(
+    {
+        name:{
+            type:String,
+            required:[true,'Activity Name is required']
+        },
+        description:{
+            type:String,
+            required:[true, 'Activity Description is required']
+       },
+        activitytype: {
+            type:String,
+            enum:['run', 'bicycle', 'swim', 'walk', 'hike'],
+            required:[false, 'Please select one Activity type']
+        },
+        duration:{
+            type:Number,
+            required:[true, 'Activity Description is required']
+        },
+        date:{
+            type:Date,
+            required:[true, 'Activity Description is required']
+        }
+
+    }
+);
+
+module.exports = mongoose.model('Activity',activitySchema);
